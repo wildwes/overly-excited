dc988434881d4f970d8dc721eca34cdb36ac5b1c
Wes Waters <weswaters@rocketmail.com>
2020-04-22 15:10:22 -0500

    semi-finished

---
+++ overly-excited.js
@@ -0,0 +1,28 @@
+// Create an array that contains the words in the sentence
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+//
+//     The addExcitement function should be an impure function, and accept
+//     the array as the sole argument. It should iterate over the array
+//     and output the words to the browser console.
+const wordCount = 1;
+
+function addExcitement (theWordArray) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
+    let buildMeUp = ""
+    
+    for (let i =0; i < theWordArray.length; i++) {
+        // conctenate the new word onto buildMeUp
+        buildMeUp += `${theWordArray[i]} `
+        // putting a space between words⬆
+        // print buildMeUp to the console
+        console.log(buildMeUp)
+        
+    }
+   
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence)
+
---
+++ overly-overly-excited.js
@@ -0,0 +1,37 @@
+// Create an array that contains the words in the sentence
+let sentence1 = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+//
+//     The addExcitement function should be an impure function, and accept
+//     the array as the sole argument. It should iterate over the array
+//     and output the words to the browser console.
+let wordCount1 = 1;
+
+function addExcitement1 (theWordArray) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
+    let buildMeUp = ""
+    
+    for (let i =0; i < theWordArray.length; i++) {
+        // conctenate the new word onto buildMeUp
+        if(
+            wordCount1 % 3 === 0
+            ){
+                buildMeUp += `${theWordArray[i]}! `;
+                wordCount1++;
+            }
+            else{
+                buildMeUp += `${theWordArray[i]} `;
+                // putting a space between words⬆
+                wordCount1++;
+        }
+        // print buildMeUp to the console
+        console.log(buildMeUp)
+        
+    }
+   
+}
+
+// Invoke the function and pass in the array
+addExcitement1(sentence1)
+
